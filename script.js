7f71a9dc267c1b1c2bc56e865de1a6b0387bb9dc
Mark <marklajom.work@gmail.com>
2023-02-17 17:21:08 +0100

    Add css and hamburger script functionalities

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger")
+const mobileMenu = document.querySelector(".sliding")
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active")
+  mobileMenu.classList.toggle("visible")
+})
