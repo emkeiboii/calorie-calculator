4c11b393b381478633d9cdaadc9b583465776aef
Mark <marklajom.work@gmail.com>
2023-02-22 15:42:53 +0100

    Add javascript basic functionalities

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const hamburger = document.querySelector(".hamburger")
 const mobileMenu = document.querySelector(".sliding")
+const calcButton = document.getElementById("calculate")
+const clearButton = document.getElementById("clear")
+
+
+
+
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active")
   mobileMenu.classList.toggle("visible")
 })
+
+calcButton.addEventListener('click', calculateBMI)
+
+function calculateBMI(){
+    let age = document.getElementById('age').value,
+    height = document.getElementById('cm').value,
+    weight = document.getElementById('kg').value,
+    gender = document.querySelector('#bmi-si input[name = "gender"]:checked').value;
+  console.log(age, height, weight, gender)
+}
