ce20f56c6603ca8b00b4f1c239d7ed25a1755874
Mark <marklajom.work@gmail.com>
2023-02-23 16:34:14 +0100

    Add all functionalities of the website

--- script.js
+++ script.js
@@ -12,12 +12,68 @@ hamburger.addEventListener("click", () => {
   mobileMenu.classList.toggle("visible")
 })
 
-calcButton.addEventListener('click', calculateBMI)
+calcButton.addEventListener('click', performBMIcalc)
+clearButton.addEventListener('click', clearCalc)
 
-function calculateBMI(){
-    let age = document.getElementById('age').value,
-    height = document.getElementById('cm').value,
-    weight = document.getElementById('kg').value,
-    gender = document.querySelector('#bmi-si input[name = "gender"]:checked').value;
-  console.log(age, height, weight, gender)
+function clearCalc(){
+  let reset = document.getElementById('BMI-form').reset()
+}
+
+function performBMIcalc(){
+  let BMIInfo = getUserInfo()
+  if(BMIInfo) printBMIResult(BMIInfo)
+}
+
+
+function getUserInfo(){
+  let status
+  let age = document.getElementById('age').value,
+  height = document.getElementById('cm').value,
+  weight = document.getElementById('kg').value,
+  gender = document.querySelector('#bmi-si input[name = "gender"]:checked').value;
+  
+
+  status = checkInputStatus([age, height, weight])
+  
+  if(status == true){
+    return calculateBMI({
+      gender,
+      age,
+      height: parseFloat(height) / 100,
+      weight: parseFloat(weight)
+    })
+  } else{
+    alert('please correctly fill all the fields')
+  }
+}
+
+function checkInputStatus(inputs){
+  for(let i = 0; i < inputs.length; i++){
+      if(inputs[i].trim() === "" || isNaN(inputs[i]))
+      return false;
+  }
+  return true;
+}
+
+function calculateBMI(values){
+  let BMI = (values.weight / Math.pow(values.height, 2)).toFixed(2);
+  return{gender: values.gender, BMI} 
+}
+
+function printBMIResult(BMIInfo){
+  document.getElementById('bmi-value').innerHTML = `${BMIInfo.BMI} kg/m<sup>2</sup>`;
+
+  let bmiCategory;
+  if(BMIInfo.BMI < 18.5){
+      bmiCategory = "Underweight";
+  } else if(BMIInfo.BMI >= 18.5 && BMIInfo.BMI <= 24.9){
+      bmiCategory = "Normal Weight";
+  } else if(BMIInfo.BMI >= 25 && BMIInfo.BMI <= 29.9){
+      bmiCategory = "Overweight";
+  } else {
+      bmiCategory = "Obesity";
+  }
+
+  document.getElementById('bmi-category').innerHTML = `${bmiCategory}`;
+  document.getElementById('bmi-gender').innerHTML = BMIInfo.gender;
 }
